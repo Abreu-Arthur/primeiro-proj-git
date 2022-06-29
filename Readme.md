db67c2fbadbfe0e400dc805d2202c508ddaba4a9
Abreu-Arthur <arthurabreuejulia@gmail.com>
2022-06-29 11:02:13 -0300

    primeiro-proj-github

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Olá, esse projeto é meu aprendizado sobre git, não merece ser analisado
+Thanks
---
+++ script.js
@@ -0,0 +1,4 @@
+function bot(){
+window.open("https://www.youtube.com/watch?v=eJG7PaLOqe0&t=3s","_blank","width:350px;height:350px;left:50px;",true)
+}
+//setInterval(bot,10000)
